e94e560c1d95106d6a26165a1d0c8616da92679f
RafikMuradyan <muradyanrafik1@gmail.com>
2024-11-11 23:41:20 +0400

    fix

--- database/ormconfig.ts
+++ database/ormconfig.ts
@@ -1,9 +1,12 @@
+/* eslint-disable prefer-template */
 // eslint-disable-next-line @typescript-eslint/no-require-imports
 require('dotenv').config();
 import { DataSource, DataSourceOptions } from 'typeorm';
 import { databaseConfigSchema } from '../src/utils';
-import { UserEntity } from '../src/modules/user/user.entity';
-import { FriendRequestEntity } from '../src/modules/friend-request/friend-request.entity';
+import type { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+const migrations = [__dirname + `/database/migrations/*{.ts,.js}`];
+const entities = [__dirname + '/../src/modules/**/*.entity{.ts,.js}'];
 
 export const dataSourceOptions: DataSourceOptions = {
   type: 'postgres',
@@ -17,14 +20,27 @@ export const dataSourceOptions: DataSourceOptions = {
       ? { rejectUnauthorized: false }
       : undefined,
   synchronize: false,
-  entities: [UserEntity, FriendRequestEntity],
-  migrations: ['dist/database/migrations/*.js'],
+  entities,
+  migrations,
   subscribers: [],
 };
 
 const { error } = databaseConfigSchema.validate(dataSourceOptions);
 
 if (error) throw new Error(`Invalid database configuration: ${error.message}`);
+
+export const typeOrmConfig: TypeOrmModuleOptions = {
+  type: 'postgres',
+  entities,
+  migrations,
+  name: 'default',
+  host: process.env.POSTGRES_HOST,
+  port: parseInt(process.env.POSTGRES_PORT, 10),
+  username: process.env.POSTGRES_USERNAME,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DATABASE,
+  migrationsRun: true,
+};
 
 const dataSource = new DataSource(dataSourceOptions);
 export default dataSource;
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { dataSourceOptions } from '../database/ormconfig';
+import { typeOrmConfig } from '../database/ormconfig';
 import { AuthModule } from './modules/auth/auth.module';
 import { FriendRequestModule } from './modules/friend-request/friend-request.module';
 import { UserModule } from './modules/user/user.module';
@@ -9,7 +9,7 @@ import { UserModule } from './modules/user/user.module';
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    TypeOrmModule.forRoot(dataSourceOptions),
+    TypeOrmModule.forRoot(typeOrmConfig),
     AuthModule,
     UserModule,
     FriendRequestModule,
